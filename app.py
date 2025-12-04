fe3c7f09ebc4fd4c3da6e5734c5d066ea5820ad9
artexpury925 <artexpury@gmail.com>
2025-12-04 16:34:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,58 +2,41 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from models import db, User
 from config import Config
-from mega_storage import MegaStorage
-
-# Initialize MEGA right after creating the app
-@app.before_first_request
-def initialize_mega():
-    MegaStorage()  # will login once with your account
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
+login_manager = LoginManager(app)
 login_manager.login_view = 'login'
-login_manager.login_message_category = "info"
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-@app.before_first_request
-def create_tables():
+# Create database + tables automatically
+with app.app_context():
     db.create_all()
 
 @app.route('/')
 def index():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
     return redirect(url_for('login'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form['username']
-        email = request.form['email']
         password = request.form['password']
 
         if User.query.filter_by(username=username).first():
-            flash('Username already exists!', 'danger')
-            return redirect(url_for('register'))
-
-        if User.query.filter_by(email=email).first():
-            flash('Email already registered!', 'danger')
-            return redirect(url_for('register'))
-
-        user = User(username=username, email=email)
-        user.set_password(password)
-        db.session.add(user)
-        db.session.commit()
-
-        flash('Account created! Please log in.', 'success')
-        return redirect(url_for('login'))
+            flash('Username already taken!', 'danger')
+        else:
+            user = User(username=username)
+            user.set_password(password)
+            db.session.add(user)
+            db.session.commit()
+            flash('Account created! Login now', 'success')
+            return redirect(url_for('login'))
 
     return render_template('register.html')
 
@@ -68,21 +51,21 @@ def login():
             login_user(user)
             return redirect(url_for('home'))
         else:
-            flash('Invalid username or password', 'danger')
+            flash('Wrong username or password', 'danger')
 
     return render_template('login.html')
 
 @app.route('/home')
 @login_required
 def home():
-    return render_template('home.html')
+    return render_template('home.html', username=current_user.username)
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    flash('Logged out successfully!', 'info')
+    flash('Logged out!', 'info')
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
